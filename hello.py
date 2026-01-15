51e26bca97214406eff1e98886c3c70919f2b9d0
Saket-Rama <163721169+Saket-Rama@users.noreply.github.com>
2026-01-15 19:27:04 +0530

    for loop initialised

--- hello.py
+++ hello.py
@@ -1,4 +1,9 @@
 for i in range(6):
-    for j in range(i):
+    for j in range(1,i+1):
         print("*",end=" ")
+    print()
+
+for i in range(6,0,-1):
+    for j in range(1,i+1):
+        print("*", end=" ")
     print()
